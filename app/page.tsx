1e96ea5ead36fa69c76da1ea040f9386e1aed2d7
shaheenshay <shaheen.shay@gmail.com>
2024-08-21 12:46:00 +0530

    footer section updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,11 @@ import Hero from "@/components/Hero";
 import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}        
         />
